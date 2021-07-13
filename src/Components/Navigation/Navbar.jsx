539212ab4e6c9041999d0b6e4a5df35c340e96f8
Kheyyon <parktwins1@gmail.com>
2021-07-13 12:52:07 -0400

    I made an exact path to home so content only displays on the home page and I made a calculator link to visualize the concept

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
+    <div>
     <Link to="/">Home</Link>
+    <Link to="/calculator">Calculator</Link>
+    </div>
   )
 }
