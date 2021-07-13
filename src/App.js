945f9314bf0b0f756f43dd4d8b1909b7d5f1eeae
Kheyyon <parktwins1@gmail.com>
2021-07-13 12:35:51 -0400

    I added react materialize and created folders for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
       </header>
     </div>
   );
 }
 
 export default App;
+
+{/* What would I need to make my component hierarchy real?
+
+1.) Components folder
+2.) Subfolders: Header, Navigation, Pages, Content, Footer
+3.) Let's build out our header bar 
+
+*/}
+
