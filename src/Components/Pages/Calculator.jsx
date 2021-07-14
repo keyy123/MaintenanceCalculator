0c9940596c385eac37023b1b76e4b5ae3df1ad31
Kheyyon <parktwins1@gmail.com>
2021-07-14 18:26:11 -0400

    I got the map working by editing the airTable URL. Afterwards, I added another <div>

--- src/Components/Pages/Calculator.jsx
+++ src/Components/Pages/Calculator.jsx
@@ -1,12 +1,25 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
 
 export default function Calculator() {
+  //global variables
   const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE
   const AIRTABLE_KEY = process.env.REACT_APP_AIRTABLE_KEY
-  const baseURL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/weigh-in%20records \ `
+
+
+
+
+  const baseURL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/weigh-in%20records?sort%5B0%5D%5Bfield%5D=days\ `
   
   
+  
+  
+  let Arr2 = []
+  let Arr3 = []
+  
+ 
+  //states
   const [data, setData] = useState([])
   const [newArr, setNewArr] = useState([])
   const [toggle, setToggle] = useState(false)
@@ -14,11 +27,10 @@ export default function Calculator() {
   const [sum, setSum] = useState(0)
   const [day, setDay] = useState(0)
   const [weight, setWeight] = useState(0)
-let Arr2 = []
-let Arr3 = []
-  
+ 
 
-  
+  //functions
+
   function sumKcal(x) {
     x?.map((log) => {
       setNewArr((newArr) => [...newArr, log.fields.kcal])
@@ -33,7 +45,7 @@ let Arr3 = []
     }
     )
     setDay(Math.max(...Arr2))
-}
+  }
 
   function weightChange(x) {
     x?.map((log) => {
@@ -42,14 +54,24 @@ let Arr3 = []
     setWeight(Arr3[0] - (Arr3[Arr3.length - 1]))
   }
 
-  console.log(Arr2)
+  function sortDay(x) {
+    x?.map((log) => {
+      Arr2.push(log.fields.days)
+    })
+
+  }
   
+  
+  
+  
+  //UseEffects
+
   useEffect(() => {
     async function apiCall() {
       const res = await axios.get(baseURL, { headers: { Authorization: `Bearer ${AIRTABLE_KEY}` } })
       setData(res.data.records)
       // sumKcal(data)
-      setToggle(prevToggle => !prevToggle)  
+      setToggle(prevToggle => !prevToggle)
     }
     apiCall()
    
@@ -75,28 +97,30 @@ let Arr3 = []
   //  console.log(sum)
 
   
-    function calculation() {
+  function calculation() {
     let averageIntake = Math.floor(sum / day)
-      let maintenanceAdjustmentPercent = weight / day
-      const onePound = 3500
-      const kcalAdjustment = Math.abs((maintenanceAdjustmentPercent)) * onePound
-      const maintenanceIntake = kcalAdjustment + (averageIntake) 
-      return maintenanceIntake 
-}
+    let maintenanceAdjustmentPercent = weight / day
+    const onePound = 3500
+    const kcalAdjustment = Math.abs((maintenanceAdjustmentPercent)) * onePound
+    const maintenanceIntake = kcalAdjustment + (averageIntake)
+    return maintenanceIntake
+  }
 
-
+  
 
   return (
-    <div>
-      {/* <p>{sum > 0 ? `The sum of your daily intakes is ${sum}` : null}</p>
-      <p>{data.length > 0 ? `The number of days at this intake is: ${day}` : null }</p>
-      <p>{`Average daily intake is: ${Math.floor(sum / day)}`}</p>
-      <p>{`Weight change for the period is: ${weight}`}</p> */}
-      <p>{`${data.fields.days} - ${data.fields.lbs}`}</p>
+  <div>
+  <div className = 'Columns'>
+      {data.map((log) => {
+        return (<div>{log.fields.days}, {log.fields.lbs}, {log.fields.kcal}</div>)
+        })
+      }
+    </div>  
       <p>{`This is your maintenance calories for the period: ${calculation()}`}</p>
     </div>
   )
 }
+  
 
 
 //logic still shifts between numbers...I'm going to need to fix that so its more user-friendly
