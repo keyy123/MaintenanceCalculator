6cc7a459574e85997fc5893cb0a5a99ad9579132
Kheyyon <parktwins1@gmail.com>
2021-07-13 21:11:38 -0400

    I vastly improved calculator function with Gracen's help. It is display the various bits needed to make the equation work but still some issues

--- src/Components/Pages/Calculator.jsx
+++ src/Components/Pages/Calculator.jsx
@@ -1,47 +1,106 @@
-import React from 'react'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import log from 'loglevel'
 
 export default function Calculator() {
   const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE
   const AIRTABLE_KEY = process.env.REACT_APP_AIRTABLE_KEY
   const baseURL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/weigh-in%20records \ `
-  const[data,setData] = useState([])
+  
+  
+  const [data, setData] = useState([])
+  const [newArr, setNewArr] = useState([])
+  const [toggle, setToggle] = useState(false)
+  const [toggleFetch, setToggleFetch] = useState(false)
+  const [sum, setSum] = useState(0)
+  const [day, setDay] = useState(0)
+  const [weight, setWeight] = useState(0)
+let Arr2 = []
+let Arr3 = []
+  
 
+  
+  function sumKcal(x) {
+    x?.map((log) => {
+      setNewArr((newArr) => [...newArr, log.fields.kcal])
+     
+    })
+    // setToggle(prevToggle => !prevToggle)
+  }
+
+  function highestDay(x) {
+    x?.map((log) => {
+      Arr2.push(log.fields.days)
+     
+    }
+    )
+    setDay(Math.max(...Arr2))
+}
+
+  function weightChange(x) {
+    x?.map((log) => {
+      Arr3.push(log.fields.lbs)
+    })
+    setWeight(Arr3[0] - (Arr3[Arr3.length - 1]))
+  }
+
+  console.log(Arr2)
+  
   useEffect(() => {
     async function apiCall() {
-      const res = await axios.get(baseURL, {headers:{Authorization: `Bearer ${AIRTABLE_KEY}` } })
+      const res = await axios.get(baseURL, { headers: { Authorization: `Bearer ${AIRTABLE_KEY}` } })
       setData(res.data.records)
+      // sumKcal(data)
+      setToggle(prevToggle => !prevToggle)  
     }
     apiCall()
-  },[])
+   
+  }, [])
+
+  useEffect(() => {
+  
+    sumKcal(data)
+    highestDay(data)
+    weightChange(data)
+    setToggleFetch(prevToggleFetch => !prevToggleFetch)
+  }
+    , [toggle])
+
+  useEffect(() => {
+    if (newArr.length > 0) {
+      setSum(newArr.reduce((a, b) => a + b))
+    }
+  }, [toggleFetch])
+
+
+  //  let sum = newArr.reduce((a, b) => { a + b })
+  //  console.log(sum)
+  
+  function kcalSum(num) {
+    let total = 0;
+    total += num
+    return total
+  }
+  
+  // function calculation() {
+  //   let averageIntake = Math.floor(sum / day)
+  //   let kcalAdjustment = 3500 * ()
+
+  // }
+
 
 
   return (
     <div>
-      {data?.map((log, index) => {
-        for(let i = 0; i < index.length; i++ ){
-          const timePeriod = Math.MAX(log.fields?.days)
-          console.log(timePeriod)
-        }
-        return(
-          <div key={log.id}>
-            
-          <h2>{log.fields?.days}</h2>
-          <h2>{log.fields?.kcal}</h2>
-          <h2>{log.fields?.lbs}</h2>
-          </div>
-      )})}
+      <h1>{sum > 0 ? sum : null}</h1>
+      <p>{data.length > 0 ? day : null }</p>
+      <p>{`Average daily intake is: ${Math.floor(sum / day)}`}</p>
+      <p>{weight}</p>
     </div>
   )
 }
+//   )
+// }
+// */
+//   )
+// }
 
-//What do I need to get data from airtable into my program?
-{/* 
-  1.) I need to make a .env.development.local to store airtable base and key
-  2.) Assign them values of process.env.REACT_APP_AIRTABLE_BASE=BASE and same for key
-  3.) Make a baseURL as a variable
-  4.) Make a useEffect and useState for axios.get call 
-  5.) Map the data to page to be safe
-*/ }
