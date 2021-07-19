bf9f5ce69cf54c5efa049db729f2c52ea4a7c384
Kheyyon <parktwins1@gmail.com>
2021-07-19 12:44:23 -0400

    I got the home page on 280px working with words in front of blur now I need to do the same for the desktop

--- src/Components/Navigation/Modal.jsx
+++ src/Components/Navigation/Modal.jsx
@@ -9,6 +9,7 @@ export default function Modals() {
        <div className="background">
           <div className="content">
           <Font family="Archivo Narrow">
+          <section className="banner">
           <p> This react app's purpose is to determine the caloric
           maintenance intake of an individual based on the
           individual's weight and intake entries.
@@ -16,8 +17,8 @@ export default function Modals() {
           An individual who is aware of their body's maintenance
           intake is able to quickly form a meal plan for cutting(weight
           loss), bulking (weight gain) or even indefinite maintenance.
-        
-        </p>
+              </p>
+            
         <p>
         This calculator was inspired by the many people who've asked me
         too many times, "So what do I need to do to lose weight?". The
@@ -25,14 +26,17 @@ export default function Modals() {
         lower it a bit". This calculator will definitely be used for any
         client that works with me in any capacity. For that client,
         This one's for you. 
-             </p>
+              </p>
+            
               <p>
       Here are instructions for the calculator. You enter a
-      a weight in lbs and an calorie intake. It will then
-      return a maintenance kcal value which you can try yourself.
-      Be warned that the less entries posted the less accuracy This
-                calculator has.
-             </p>
+                a weight in lbs and an calorie intake per entry. Each
+                entry represents an entire day so don't worry about it.
+                The calculator will return a maintenance value based on the
+                data from each entry which you can use to meal prep however
+                you would like.
+              </p>
+              </section>
             </Font>
           </div>
        </div>
