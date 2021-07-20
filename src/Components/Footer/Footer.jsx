c931d2867339745b7fddc142ebbc61cfa16cb429
Kheyyon <parktwins1@gmail.com>
2021-07-20 09:33:16 -0400

    I adjusted table a bit for netlify and the icons on bottom of the page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,10 +14,12 @@ export default function Footer() {
            
             <p id="contact-info"> Contact me at the following:<br/>Irvington NJ <br/> 973-123-4567 <br /> hi</p>
             <p id="contact-links">
-             Contact me using the links below
+              Contact me using the links below
+              <div className = "links">
               <a href="https://github.com/keyy123/MaintenanceCalculator/"><AiFillGithub value={{ className: 'react-icons GH' }} /></a>
               <a href="https://www.linkedin.com/in/kheyyon-parker-b98739179/"><AiFillLinkedin value={{className: 'react-icons LI'}}/></a>
               <a href="mailto:kheyyon.parker@gmail.com"><AiFillMail value={{ className: 'react-icons mail' }} /></a>
+            </div>
             </p>
             <p id="something">something</p>
           </div>
